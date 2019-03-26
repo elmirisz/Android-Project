df835775d0bbf5adbd12682f467e8da54f5c702c
Elmir Šut <elmirsutisz@gmail.com>
2019-03-26 04:49:52 +0100

    projekat

---
+++ app/src/main/java/com/homework/realorfake/InstructionActivity.java
@@ -0,0 +1,16 @@
+package com.homework.realorfake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InstructionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instruction);
+    }
+
+
+
+}
