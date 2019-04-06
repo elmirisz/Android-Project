68c79822d5220ee1f5f358768be8caf1127f5149
Elmir Šut <elmirsutisz@gmail.com>
2019-04-06 22:06:31 +0200

    Adding new activity that will be main part of the application named GameActivity

--- app/src/main/java/com/homework/realorfake/InstructionActivity.java
+++ app/src/main/java/com/homework/realorfake/InstructionActivity.java
@@ -2,6 +2,8 @@ package com.homework.realorfake;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class InstructionActivity extends AppCompatActivity {
 
@@ -9,8 +11,13 @@ public class InstructionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instruction);
-    }
 
+    }
+    public void startGame(View view)
+    {
+        Intent intent = new Intent(InstructionActivity.this, GameActivity.class);
+        startActivity(intent);
+    }
 
 
 }
