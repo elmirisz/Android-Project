f4fb5b11c41f29e91d0aba62a1baa92d0c041451
Elmir Šut <elmirsutisz@gmail.com>
2019-04-09 00:56:49 +0200

    Created java code to avoid crash when temp 0

--- app/src/main/java/com/homework/realorfake/PopUp.java
+++ app/src/main/java/com/homework/realorfake/PopUp.java
@@ -57,7 +57,10 @@ public class PopUp extends Activity {
 
         //Adding images to PopUp activity
         image1 = findViewById(R.id.changePhotoPop);
-        image1.setImageResource(photos[temp]);
+        if(temp==0){
+            image1.setImageResource(photos[4]);
+        }else{
+        image1.setImageResource(photos[temp]);}
 
 
 
