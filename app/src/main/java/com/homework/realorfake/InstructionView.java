0127f7d70102586f4eede00c71f0c188a4272991
HasanM9 <mahmutagichasan05@gmail.com>
2019-04-16 23:53:18 +0200

    gifa processing

--- app/src/main/java/com/homework/realorfake/InstructionView.java
+++ app/src/main/java/com/homework/realorfake/InstructionView.java
@@ -10,7 +10,11 @@ import android.widget.VideoView;
 
 public class InstructionView extends Activity {
 
+
+    //Here we define the uri, which we use for video instruction
     Uri uri;
+
+    //Here we define the videov, which we use for video instruction
     VideoView videov;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,19 +29,26 @@ public class InstructionView extends Activity {
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
+        //window width
         int width = dm.widthPixels;
+
+        //window height
         int height = dm.heightPixels;
 
         //here we set the percentage of pop-up window of phones resolution
         getWindow().setLayout((int)(width * .95), (int)(height * .75));
 
 
-
+        //call the videoView we've created in instruction_view.xml
         videov = (VideoView) findViewById(R.id.videoView);
 
+        //We call the resource where the video is located
         uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video);
 
+        //we connect the videoView and resource where the video is located
         videov.setVideoURI(uri);
+
+        //a call to get the video started
         videov.start();
 
 
